aa17888ca685bca90ad35b8006b6592cf6df7380
Simon Lentz <simonemlentz@gmail.com>
2025-11-23 11:48:28 +0100

    inc version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule CubDB.Mixfile do
 
   @source_url "https://github.com/zteln/cubdb"
   @forked_url "https://github.com/lucaong/cubdb"
-  @version "2.0.5"
+  @version "2.0.6"
 
   def project do
     [
